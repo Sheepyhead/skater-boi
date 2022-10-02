d0ae35dcdbc8dcbe313959c2b01f9c1c87e1e36b
Troels Peter Jessen <trojes@tuta.io>
2022-10-02 22:42:13 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic, clippy::cargo)]
+#![allow(
+    clippy::module_name_repetitions,
+    clippy::cargo_common_metadata,
+    clippy::type_complexity,
+    clippy::too_many_arguments,
+    clippy::needless_pass_by_value,
+    clippy::multiple_crate_versions,
+    clippy::cast_possible_truncation,
+    clippy::cast_precision_loss,
+    clippy::too_many_lines,
+    clippy::similar_names,
+    clippy::must_use_candidate,
+    clippy::enum_glob_use
+)]
+
+use bevy::{prelude::*, window::PresentMode};
+use bevy_rapier3d::prelude::*;
+use debug::Debug;
+
+mod debug;
+
+pub const CLEAR: Color = Color::BLACK;
+pub const HEIGHT: f32 = 600.0;
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: HEIGHT * RESOLUTION,
+            height: HEIGHT,
+            title: "Skater Boi".to_string(),
+            present_mode: PresentMode::Fifo,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        // External plugins
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
+        // Internal plugins
+        .add_plugin(Debug)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_ground)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = Camera3dBundle::default();
+
+    camera.transform.translation = [-3.0, 2.0, 0.0].into();
+    camera.transform.look_at(Vec3::X, Vec3::Y);
+
+    commands.spawn_bundle(camera);
+}
+
+fn spawn_ground(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpatialBundle::default())
+        .insert_bundle((Collider::cuboid(100.0, 0.1, 100.0), RigidBody::Fixed));
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpatialBundle { ..default() })
+        .insert_bundle((Collider::cylinder(0.5, 0.25), RigidBody::Dynamic));
+}
